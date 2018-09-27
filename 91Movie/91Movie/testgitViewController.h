3211956b2fa9668e57f75d6588862b55325809c5
Matcha00 <chmatcha00@gmail.com>
2018-09-28 02:35:44 +0800

    test

---
+++ 91Movie/91Movie/testgitViewController.h
@@ -0,0 +1,13 @@
+//
+//  testgitViewController.h
+//  91Movie
+//
+//  Created by 陈欢 on 2018/9/28.
+//  Copyright © 2018年 陈欢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testgitViewController : UIViewController
+
+@end
---
+++ 91Movie/91Movie/testgitViewController.m
@@ -0,0 +1,37 @@
+//
+//  testgitViewController.m
+//  91Movie
+//
+//  Created by 陈欢 on 2018/9/28.
+//  Copyright © 2018年 陈欢. All rights reserved.
+//
+
+#import "testgitViewController.h"
+
+@interface testgitViewController ()
+
+@end
+
+@implementation testgitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
